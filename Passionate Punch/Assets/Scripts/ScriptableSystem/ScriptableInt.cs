e60b809bdc9798519241ba6790f063c4e7301004
YusaCan <ahmedyusacan@gmail.com>
2022-10-31 11:38:40 +0300

    Scriptable System Created
    Scriptable Bool, Int and Float to use them effectively

---
+++ Passionate Punch/Assets/Scripts/ScriptableSystem/ScriptableInt.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableSystem/ScriptableInt" , fileName = "ScriptableInt")]
+public class ScriptableInt : ScriptableObject
+{
+    public int value;
+}
