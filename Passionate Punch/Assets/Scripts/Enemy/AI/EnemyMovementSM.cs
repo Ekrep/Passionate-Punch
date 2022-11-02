c932139f98a7e3647f5f6898a5cddb03df671d64
YusaCan <ahmedyusacan@gmail.com>
2022-11-02 17:34:12 +0300

    EnemyMovementSM Script Updated
    Enemy Return State Integrated to the state machine

--- Passionate Punch/Assets/Scripts/Enemy/AI/EnemyMovementSM.cs
+++ Passionate Punch/Assets/Scripts/Enemy/AI/EnemyMovementSM.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyMovementSM : EnemyStateMachine
 {
     public ScriptableFloat enemyMovementSpeed;
+    public Transform enemyCampPos;
     // Our enemy game object
     [HideInInspector]
     public GameObject enemy;
@@ -15,12 +16,15 @@ public class EnemyMovementSM : EnemyStateMachine
     public EnemyMovingState enemyMovingState;
     [HideInInspector]
     public EnemyChasingState enemyChasingState;
+    [HideInInspector]
+    public EnemyReturnState enemyReturnState;
     private void Awake()
     {
         enemy = this.gameObject;
         enemyIdleState = new EnemyIdleState(this);
         enemyMovingState = new EnemyMovingState(this);
         enemyChasingState = new EnemyChasingState(this);
+        enemyReturnState = new EnemyReturnState(this);
     }
     protected override EnemyBaseState GetInitialState()
     {
