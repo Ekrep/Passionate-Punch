838279a27ed411b5b90d6101bbadf9e5a99887a8
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-11-02 14:05:19 +0300

    Deleted Character
    Deleted 1 class because we only need 2.
    Corrected inventory script

--- Passionate Punch/Assets/Scripts/InventorySystem/Inventory.cs
+++ Passionate Punch/Assets/Scripts/InventorySystem/Inventory.cs
@@ -10,21 +10,21 @@ namespace InventorySystem
     public class Inventory : MonoBehaviour
     {
         public static int inventorySize = 16; //can be changed later.
-        public static IDictionary<Item, String> inventoryDict = new Dictionary<Item, String>(inventorySize);
-
+        //public static IDictionary<Item, ItemSettings> inventoryDict = new Dictionary<Item, ItemSettings>(inventorySize);
+        public static List<Item> inventoryList = new List<Item>(inventorySize); 
         public void AddInventory(Item item)
         {
             if (CheckInventorySize())
             {
-                inventoryDict.Add(item, item.GetComponent<ItemSettings>().itemTitle);
+                inventoryList.Add(item);
+                //inventoryDict.Add(item, item.GetComponent<ItemSettings>());
                 item.PickedUp();
-
             }
         }
 
         public bool CheckInventorySize()
         {
-            return inventoryDict.Count < inventorySize;
+            return inventoryList.Count < inventorySize;
         }
     }
 }
