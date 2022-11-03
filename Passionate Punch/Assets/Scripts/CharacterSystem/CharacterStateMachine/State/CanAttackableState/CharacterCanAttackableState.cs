8d349c98d96af97fc18ec22044257d21c25c5fe2
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-11-03 17:17:32 +0300

    CharacterAttack Update

--- Passionate Punch/Assets/Scripts/CharacterSystem/CharacterStateMachine/State/CanAttackableState/CharacterCanAttackableState.cs
+++ Passionate Punch/Assets/Scripts/CharacterSystem/CharacterStateMachine/State/CanAttackableState/CharacterCanAttackableState.cs
@@ -4,45 +4,44 @@ using UnityEngine;
 
 public class CharacterCanAttackableState : CharacterAliveState
 {
-    public bool stillPressingAttack=false;
-   
-    public CharacterCanAttackableState(string name,CharacterBaseStateMachine stateMachine) : base(name, stateMachine)
+    public bool stillPressingAttack = false;
+
+    public CharacterCanAttackableState(string name, CharacterBaseStateMachine stateMachine) : base(name, stateMachine)
     {
 
     }
-   
+    public override void Enter()
+    {
+        base.Enter();
+        stillPressingAttack = false;
+    }
     public override void Update()
     {
         base.Update();
-       
-        
+
+        Attacking();
     }
     public override void LateUpdate()
     {
         base.LateUpdate();
-        Attacking();
+        
     }
 
     private void Attacking()
     {
         //UI Input gelince degisecek!!
-        if (Input.GetKey(KeyCode.Space)&&!stillPressingAttack)
+        if (Input.GetKey(KeyCode.Space) && !stillPressingAttack)
         {
             stillPressingAttack = true;
             Debug.Log("keypress");
-          
+
             sm.ChangeState(sm.characterAttackingState);
-            
-            
-        }
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            
-           stillPressingAttack = false;
-            sm.ChangeState(sm.characterIdleState);
+
+
         }
        
 
-    }
 
+    }
+  
 }
