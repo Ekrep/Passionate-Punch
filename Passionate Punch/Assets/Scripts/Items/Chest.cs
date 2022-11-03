c1a24438fc921e08fbf2e9a45fbe9e862f04e1ef
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-11-03 16:09:50 +0300

    Development of chest
    These chests will be randomly spawned on map. It will contain different amount of items. Player can reach these items by breaking the chests.

--- Passionate Punch/Assets/Scripts/Items/Chest.cs
+++ Passionate Punch/Assets/Scripts/Items/Chest.cs
@@ -2,17 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+namespace Items
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+
+
+    public class Chest : MonoBehaviour
     {
-        
+        public int itemIndex;
+        public int chestCount;
+        [SerializeField] private int maxChestCount; //Can be changed after trial and fail.
+        [SerializeField] private List<ItemSettings> allItems = new List<ItemSettings>();
+        public List<ItemSettings> chestList;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void FillChest()
+        {
+            chestCount = Random.Range(1, maxChestCount);
+            for(int i = 0; i < chestCount; i++){
+                itemIndex = Random.Range(0, allItems.Count);
+                ItemSettings tempItem = allItems[itemIndex];
+                chestList.Add(tempItem);
+                
+            }
+        }
     }
 }
