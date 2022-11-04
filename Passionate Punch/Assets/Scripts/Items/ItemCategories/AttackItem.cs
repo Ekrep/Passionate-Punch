d403754de15dd962e0c952a14755329e9f71c35a
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-11-04 16:53:35 +0300

    Update on attack item
    I wanted that some items can give player high amount of attack damage. While giving that, these items will give small amount of attack speed (I wanted to develop this basic feature for especially dagger and hammer). Therefore, player can choose items as their wish and these items will increase variety.

--- Passionate Punch/Assets/Scripts/Items/ItemCategories/AttackItem.cs
+++ Passionate Punch/Assets/Scripts/Items/ItemCategories/AttackItem.cs
@@ -7,20 +7,21 @@ namespace ItemCategories
 {
     [CreateAssetMenu(menuName = "Scriptables/Items/AttackItem")]
 
-
     public class AttackItem : ItemSettings
     {
         //Attack items increase player's attack damage and speed.
+
+        public float speedEffectAmount;
         public override void ApplyItemEffect(float amount)
         {
             player.attackDamage += amount;
-            player.attackSpeed += amount / 10;
+            player.attackSpeed += speedEffectAmount;
         }
 
         public override void RevertItemEffect(float amount)
         {
             player.attackDamage -= amount;
-            player.attackSpeed -= amount / 10;
+            player.attackSpeed -= speedEffectAmount;
         }
 
     }
