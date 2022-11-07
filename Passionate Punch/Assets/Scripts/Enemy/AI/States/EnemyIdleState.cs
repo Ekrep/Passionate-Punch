d0aaae777dc6730d2271edbaf740110ff917f3ff
YusaCan <ahmedyusacan@gmail.com>
2022-11-07 12:13:55 +0300

    Enemy Moving State Deleted
    * Code clean up.
    * Enemy moving state deleted because it is useless

--- Passionate Punch/Assets/Scripts/Enemy/AI/States/EnemyIdleState.cs
+++ Passionate Punch/Assets/Scripts/Enemy/AI/States/EnemyIdleState.cs
@@ -19,23 +19,11 @@ public class EnemyIdleState : EnemyBaseState
     public override void UpdateLogic()
     {
         base.UpdateLogic();
-        if (Input.GetMouseButtonDown(0))
-        {
-            Debug.Log("Mouse Clicked On Idle State");
-        }
-        else if (Input.GetMouseButtonDown(1))
-        {
-            enemyStateMachine.ChangeState(enemyMovementSM.enemyMovingState);
-        }
     }
     // Collision Enter on Idle State
     public override void EnemyOnCollisionEnter(Collision collision)
     {
         base.EnemyOnCollisionEnter(collision);
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Debug.Log("Collision with Player Occured On Idle State");
-        }
     }
     // IF the player enters the trigger of the enemy, enemy starts to chasing him. (After some preparation time)
     public override void EnemyTriggerEnter(Collider other)
