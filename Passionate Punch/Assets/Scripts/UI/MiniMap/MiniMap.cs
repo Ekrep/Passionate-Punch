aebe3d4cf24a60ff345b2b2510c3ffaf45b2c4da
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-11-10 00:28:22 +0300

    Basic Minimap Completed

--- Passionate Punch/Assets/Scripts/UI/MiniMap/MiniMap.cs
+++ Passionate Punch/Assets/Scripts/UI/MiniMap/MiniMap.cs
@@ -21,7 +21,7 @@ public class MiniMap : MonoBehaviour
     void Start()
     {
         //ratio=CalculateRatio(mapLeftCorner3D.position,mapRightCorner3D.position,mapRightDownCorner3D.position,mapLeftCorner2D.localPosition,mapRightCorner2D.localPosition,mapRightDownCorner2D.localPosition);
-        CalculateRatio(mapLeftCorner3D.position,mapRightCorner3D.position,mapRightDownCorner3D.position,mapLeftCorner2D.position,mapRightCorner2D.position,mapRightDownCorner2D.position);
+        CalculateRatio(mapLeftCorner3D.position,mapRightCorner3D.position,mapRightDownCorner3D.position,mapLeftCorner2D.localPosition,mapRightCorner2D.localPosition,mapRightDownCorner2D.localPosition);
         //playerInMap.localPosition = player.transform.position * ratio;
         
 
@@ -31,7 +31,9 @@ public class MiniMap : MonoBehaviour
 
     void Update()
     {
-        playerInMap.localPosition = new Vector2(player.position.x * 1/ratioX, player.position.z * 1/ratioY);
+        //CalculateRatio(mapLeftCorner3D.position, mapRightCorner3D.position, mapRightDownCorner3D.position, mapLeftCorner2D.localPosition, mapRightCorner2D.localPosition, mapRightDownCorner2D.localPosition);
+        playerInMap.localPosition = new Vector2(-player.position.x * (ratioX), -player.position.z * (ratioY));
+        
     }
 
 
@@ -40,17 +42,17 @@ public class MiniMap : MonoBehaviour
     private void CalculateRatio(Vector3 upLeftCorner,Vector3 upRightCorner,Vector3 rightDownCorner, Vector2 miniMapUpLeftCorner, Vector2 miniMapUpRightCorner, Vector2 miniMapRightDownCorner)
     {
         float realWorldXLenght;
-        realWorldXLenght = Mathf.Abs(upLeftCorner.x - upRightCorner.x);
+        realWorldXLenght = Vector3.Distance(upLeftCorner, upRightCorner);//Mathf.Abs(upLeftCorner.x - upRightCorner.x);
         float realWorldYLenght;
-        realWorldYLenght = Mathf.Abs(upRightCorner.z - rightDownCorner.z);
+        realWorldYLenght = Vector3.Distance(upRightCorner, rightDownCorner);//Mathf.Abs(upRightCorner.z - rightDownCorner.z);
 
         float miniMapXLenght;
-        miniMapXLenght = Mathf.Abs(miniMapUpLeftCorner.x - miniMapUpRightCorner.x);
+        miniMapXLenght = Vector2.Distance(miniMapUpLeftCorner, miniMapUpRightCorner);//Mathf.Abs(miniMapUpLeftCorner.x - miniMapUpRightCorner.x);
         float miniMapYLenght;
-        miniMapYLenght = Mathf.Abs(miniMapUpRightCorner.y - miniMapRightDownCorner.y);
+        miniMapYLenght = Vector2.Distance(miniMapUpRightCorner, miniMapRightDownCorner);//Mathf.Abs(miniMapUpRightCorner.y - miniMapRightDownCorner.y);
 
-        ratioX = realWorldXLenght / miniMapXLenght;
-        ratioY = realWorldYLenght / miniMapYLenght;
+        ratioX = miniMapXLenght / realWorldXLenght;
+        ratioY = miniMapYLenght / realWorldYLenght;
 
 
 
