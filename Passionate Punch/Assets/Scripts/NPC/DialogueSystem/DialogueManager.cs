34481935d2691324c29f7b4162bffbc124779c9b
YusaCan <ahmedyusacan@gmail.com>
2022-11-18 13:24:41 +0300

    DialogueManager Script Updated
    Selective button feature integrated to the manager

--- Passionate Punch/Assets/Scripts/NPC/DialogueSystem/DialogueManager.cs
+++ Passionate Punch/Assets/Scripts/NPC/DialogueSystem/DialogueManager.cs
@@ -2,16 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 public class DialogueManager : MonoBehaviour
 {
     Queue<string> sentences;
+    List<bool> selectives;
     public ScriptableBool isInDialogue;
-    public TextMeshProUGUI nameText, dialogueText;
+    public TextMeshProUGUI nameText, dialogueText, choosingButtonText_1, choosingButtonText_2;
     public Animator dialogueAnimator;
+    public Button choosingButton_1, choosingButton_2;
 
     void Start()
     {
         sentences = new Queue<string>();
+        selectives = new List<bool>();
     }
     private void Update()
     {
@@ -25,12 +29,34 @@ public class DialogueManager : MonoBehaviour
         nameText.text = dialogue.name;
         dialogueAnimator.SetBool("IsOpen", true);
         sentences.Clear();
+        selectives.Clear();
         foreach (string sentence in dialogue.sentences)
         {
             sentences.Enqueue(sentence);
         }
-
+        foreach (bool selective in dialogue.isSelective)
+        {
+            selectives.Add(selective);
+        }
         DisplayNextSentence();
+    }
+    public void DisplaySelectiveButtons()
+    {
+        if (selectives[Dialogue.index])
+        {
+            string sentenceOfOurs_1 = sentences.Dequeue();
+            choosingButton_1.gameObject.SetActive(true);
+            choosingButtonText_1.text = sentenceOfOurs_1;
+            choosingButton_2.gameObject.SetActive(true);
+            string sentenceOfOurs_2 = sentences.Dequeue();
+            choosingButtonText_2.text = sentenceOfOurs_2;
+        }
+        else
+        {
+            choosingButton_1.gameObject.SetActive(false);
+            choosingButton_2.gameObject.SetActive(false);
+        }
+        Dialogue.index++;
     }
     public void DisplayNextSentence()
     {
@@ -43,6 +69,7 @@ public class DialogueManager : MonoBehaviour
         dialogueText.text = sentence;
         StopAllCoroutines();
         StartCoroutine(TypeSentence(sentence));
+        DisplaySelectiveButtons();
     }
     IEnumerator TypeSentence(string sentence)
     {
@@ -62,5 +89,6 @@ public class DialogueManager : MonoBehaviour
         dialogueAnimator.SetBool("IsOpen", false);
         // When conversation ends, interaction button activates again
         UIManager.Instance.interactionButton.interactable = true;
+        Dialogue.index = 0;
     }
 }
