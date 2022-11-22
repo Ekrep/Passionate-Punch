7af6641a3d803140eb406fb38a79d869ea7b3228
YusaCan <ahmedyusacan@gmail.com>
2022-11-22 14:46:20 +0300

    Clean Up For Test Dialogue

--- Passionate Punch/Assets/Dialogue/BlackMarket.ink
+++ Passionate Punch/Assets/Dialogue/BlackMarket.ink
@@ -1,1 +1,20 @@
-﻿
+    Kardeş, sen neye baktın?
+        + [Hiç bakıyorum öyle]
+            -> isFirstTrue
+        + [(Ters çıkış)]
+            -> isFirstFalse
+
+=== isFirstTrue ===
+Neye bakıyon lan kurabiye var simit var neye bakıyon
+    + [Manyak mısın sen be?]
+        -> isSecondTrue
+    + [Özür dilerim hemen gidiyorum.]
+        -> isFirstFalse
+-> END
+=== isFirstFalse === 
+Çık dışarı, çık!
+-> END
+
+=== isSecondTrue ===
+Lan manyaaaaaaağm
+-> END
--- Passionate Punch/Assets/Scripts/NPC/DialogueSystem/DialogueTrigger.cs
+++ Passionate Punch/Assets/Scripts/NPC/DialogueSystem/DialogueTrigger.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    public ScriptableBool isInRange;
-    private void Awake()
+    // This script is just for remember how to attach the JSON Script to the NPC that has been choosed.
+    // When new NPC and its dialogue created, the integration process is like this. 
+    // Rewrite the following lines into the dialogue trigger part of that NPC.
+    // You can look at BlackMarketEnter script to have idea of the process.
+    [Header("Ink JSON")]
+    public TextAsset inkJSON;
+    public ScriptableBool isInDialogue;
+    public void OpenBlackMarket()
     {
-        isInRange.value = false;
-        Debug.Log("Test Trigger");
+        if (!DialogueManager.GetInstance().isDialoguePlaying)
+        {
+            isInDialogue.value = true;
+            // Disable the interact button when player enters the conversation
+            UIManager.Instance.interactionButton.interactable = false;
+            // Test if the ink file gets or not
+            DialogueManager.GetInstance().EnterDialogueMode(inkJSON);
+        }
     }
 }
