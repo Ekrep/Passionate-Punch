2124a89e02e0cf7606f4e5dbedd91dd04d54077c
YusaCan <ahmedyusacan@gmail.com>
2022-11-24 17:37:09 +0300

    Scriptable String Object Created

---
+++ Passionate Punch/Assets/Scripts/ScriptableSystem/ScriptableString.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableSystem/ScriptableString", fileName = "ScriptableString")]
+public class ScriptableString : ScriptableObject
+{
+    public string value;
+}
