462655b9f6092c7570ae31d96d8fe2e208031676
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-11-25 18:02:19 +0300

    Skeleton implementation for stat texts
    I will use this script for text manipulation based on character data.

--- Passionate Punch/Assets/Scripts/UI/StatScreen/StatScreen.cs
+++ Passionate Punch/Assets/Scripts/UI/StatScreen/StatScreen.cs
@@ -1,17 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace StatPanel
 {
-
-
     public class StatScreen : MonoBehaviour
     {
+        [SerializeField] private GameObject settingsParent;
+        private List<TextMeshProUGUI> statTexts;
         // Start is called before the first frame update
         void Start()
         {
-
         }
 
         // Update is called once per frame
