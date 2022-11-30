c52e7479211b4207b55a8af28d850b19d3289072
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-11-30 19:06:49 +0300

    New Skill Adjustment(Explosive Trap)

--- Passionate Punch/Assets/Scripts/SkillSystem/Skills/Ranger/Scripts/LandMine/LandMine.cs
+++ Passionate Punch/Assets/Scripts/SkillSystem/Skills/Ranger/Scripts/LandMine/LandMine.cs
@@ -12,21 +12,68 @@ public class LandMine : MonoBehaviour
     [SerializeField]
     private ParticleSystem _explodeParticle;
 
-    
+    private bool _isTriggered;
+
+    [SerializeField]
+    private MeshRenderer _warningFresnelMat;
+
+    [SerializeField]
+    private float _signSpeed;
+
+    private List<CoroutineHandle> coroutineHandles = new List<CoroutineHandle>();
+
+    private void OnDisable()
+    {
+        _isTriggered = false;
+        for (int i = 0; i < coroutineHandles.Count; i++)
+        {
+           Timing.KillCoroutines(coroutineHandles[i]);
+        }
+    }
     private void OnEnable()
     {
-        Timing.RunCoroutine(DestroySelf());
-       
+        CoroutineHandle corot1;
+        CoroutineHandle corot2;
+        corot1=Timing.RunCoroutine(DestroySelf());
+        corot2=Timing.RunCoroutine(WarningSign());
+        coroutineHandles.Add(corot1);
+        coroutineHandles.Add(corot2);
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.TryGetComponent(out IHealth enemy))
+        if (other.gameObject.TryGetComponent(out IHealth enemy)&&!_isTriggered)
         {
             Timing.RunCoroutine(Hit());
+            _isTriggered = true;
         }
     }
 
+    IEnumerator<float> WarningSign()
+    {
+        while (gameObject.activeSelf)
+        {
+            while (_warningFresnelMat.material.GetFloat("_Alpha") > -0.3f)
+            {
+                float value = _warningFresnelMat.material.GetFloat("_Alpha");
+                value = Mathf.MoveTowards(value, -0.3f, _signSpeed);
+                _warningFresnelMat.material.SetFloat("_Alpha", value);
+                yield return Timing.WaitForOneFrame;
+            }
+            yield return Timing.WaitForOneFrame;
+            while (_warningFresnelMat.material.GetFloat("_Alpha") < 0.1f)
+            {
+                float value = _warningFresnelMat.material.GetFloat("_Alpha");
+                value = Mathf.MoveTowards(value, 0.1f, _signSpeed);
+                _warningFresnelMat.material.SetFloat("_Alpha", value);
+                yield return Timing.WaitForOneFrame;
+            }
+            yield return Timing.WaitForOneFrame;
+        }
+       
+
+    }
 
     IEnumerator<float> DestroySelf()
     {
@@ -47,7 +94,7 @@ public class LandMine : MonoBehaviour
             {
                 //needs vec3
                 _explodeParticle.Play();
-                enemyHealth.Hit(parentSkill.skillSettings.hitType, parentSkill.skillSettings.skillDamage, colliders[i].gameObject.transform.forward * -1, 15000f);
+                enemyHealth.Hit(parentSkill.skillSettings.hitType, parentSkill.skillSettings.skillDamage, colliders[i].gameObject.transform.forward * -1, 1500f);
 
             }
 
