5391ebab37087090f1c5cee6aa7f92d40a2fb308
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-12-01 11:41:46 +0300

    Events for communication between health bar and data

--- Passionate Punch/Assets/Scripts/CharacterSystem/CharacterHealth.cs
+++ Passionate Punch/Assets/Scripts/CharacterSystem/CharacterHealth.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Interfaces;
 using SkillSystem;
+using System;
 
 namespace CharacterSystem
 {
@@ -10,10 +11,12 @@ namespace CharacterSystem
 
     public class CharacterHealth : MonoBehaviour, IPlayerHealth
     {
+        public static event Action<float> OnTakeDamage;
+        public static event Action OnHealthRecovery;
         // This Action (OnPlayerDead) will invoke when player's health is less then 0. Then the killself function can trigger
         private void OnEnable()
         {
-            HealthBar.OnPlayerDead += KillSelf;    
+            HealthBar.OnPlayerDead += KillSelf;
         }
         private void OnDisable()
         {
@@ -38,6 +41,7 @@ namespace CharacterSystem
         public void DecreaseHealth(float amount)
         {
             this.Health -= amount;
+            OnTakeDamage?.Invoke(this.Health);
             if (this.Health <= 0)
                 KillSelf();
         }
@@ -47,24 +51,27 @@ namespace CharacterSystem
             Debug.Log("YOU DIED");
         }
 
-     
+
         void Start()
         {
 
         }
 
-       
+
         void Update()
         {
             if (canRecover && this.Health < _Character.characterStats.maxHealth)
             {
                 if (isPeriodPassed)
+                {
                     this.Health += _Character.characterStats.healthRecoveryAmount;
                     lastRecoveredTime = Time.time;
+                    OnHealthRecovery?.Invoke();
+                }
             }
         }
 
-        public void Hit(SkillSettings.HitType hitType, float damage, Vector3 hitPos,float pushAmount)
+        public void Hit(SkillSettings.HitType hitType, float damage, Vector3 hitPos, float pushAmount)
         {
             switch (hitType)
             {
@@ -73,7 +80,7 @@ namespace CharacterSystem
                     break;
                 case SkillSettings.HitType.Medium:
                     DecreaseHealth(damage);
-                    _Character.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(-hitPos.x * pushAmount * Time.deltaTime, 0,-hitPos.z * pushAmount * Time.deltaTime) , hitPos);
+                    _Character.gameObject.GetComponent<Rigidbody>().AddForceAtPosition(new Vector3(-hitPos.x * pushAmount * Time.deltaTime, 0, -hitPos.z * pushAmount * Time.deltaTime), hitPos);
                     break;
                 case SkillSettings.HitType.Hard:
                     DecreaseHealth(damage);
