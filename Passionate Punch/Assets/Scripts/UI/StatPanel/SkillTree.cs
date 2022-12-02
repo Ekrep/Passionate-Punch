95ec56de6b2490f91b97db622e53d3e7f8cf46bd
Ahmet Hakan Hafif <ahafif17@ku.edu.tr>
2022-12-02 16:30:16 +0300

    Skill images for assasin and ranger

---
+++ Passionate Punch/Assets/Scripts/UI/StatPanel/SkillTree.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using CharacterSystem;
+
+public class SkillTree : MonoBehaviour
+{
+    [SerializeField] private List<Image> skillTreeSlots;
+
+    [SerializeField] private CharacterSettings _Character;
+
+
+     void GameManager_OnSendCharacter(CharacterBaseStateMachine obj){
+            _Character = obj.characterStats;
+            for(int i = 0; i < skillTreeSlots.Count; i++)
+            {
+                skillTreeSlots[i].sprite = _Character.skillList[i].skillSprite;
+            }
+        }
+        void OnEnable()
+        {
+            GameManager.OnSendCharacter += GameManager_OnSendCharacter;
+        }
+
+        void OnDisable()
+        {
+            GameManager.OnSendCharacter -= GameManager_OnSendCharacter;
+        }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
