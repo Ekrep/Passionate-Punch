cce32dbe5cbc2879a279180f0bea0ad723d7e618
Ekrep <76533884+Ekrep@users.noreply.github.com>
2022-12-04 23:58:07 +0300

    Start Scene Adjustment

---
+++ Passionate Punch/Assets/Scripts/StartSceneScripts/CharacterSelection.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public GameObject cam;
+
+    public List<Transform> camPositions;
+
+    public float moveSpeed;
+    public float rotateSpeed;
+
+    private bool isMoving;
+    private bool isRotating;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K)&&!isMoving&&!isRotating)
+        {
+            StopCoroutine(MoveCam(0));
+            StopCoroutine(RotateCam(0));
+            StartCoroutine(MoveCam(0));
+            StartCoroutine(RotateCam(0));
+        }
+        if (Input.GetKeyDown(KeyCode.L) && !isMoving && !isRotating)
+        {
+            StopCoroutine(MoveCam(0));
+            StopCoroutine(RotateCam(0));
+            StartCoroutine(MoveCam(1));
+            StartCoroutine(RotateCam(1));
+        }
+    }
+
+
+    IEnumerator MoveCam(int listIndex)
+    {
+        isMoving = true;
+        while (Vector3.Distance(cam.transform.position, camPositions[listIndex].position) > 0.1f)
+        {
+            cam.transform.position = Vector3.MoveTowards(cam.transform.position, camPositions[listIndex].position, moveSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        isMoving = false;
+    }
+    IEnumerator RotateCam(int listIndex)
+    {
+        isRotating = true;
+        while (Vector3.Distance(cam.transform.rotation.eulerAngles, camPositions[listIndex].rotation.eulerAngles) > 0.1f)
+        {
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, camPositions[listIndex].rotation, rotateSpeed * Time.deltaTime);
+            yield return new WaitForEndOfFrame();
+        }
+        isRotating = false;
+    }
+}
