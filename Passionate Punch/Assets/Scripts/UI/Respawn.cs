a50ab0babac11771cc8b6c326047288b2c540be0
YusaCan <ahmedyusacan@gmail.com>
2022-12-08 11:53:37 +0300

    Game Over Panel Created
    Respawn.OnRespawn is the action when player pressed the respawn button

---
+++ Passionate Punch/Assets/Scripts/UI/Respawn.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using CharacterSystem;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public static Action OnRespawn;
+    public GameObject respawnPanel;
+    Animator respawnPanelAnimator;
+    private void Start()
+    {
+        respawnPanel.gameObject.SetActive(false);
+        respawnPanelAnimator = respawnPanel.GetComponent<Animator>();
+    }
+    private void OnEnable()
+    {
+        CharacterHealth.OnPlayerDeath += ActivatePanel;
+    }
+    private void OnDisable()
+    {
+        CharacterHealth.OnPlayerDeath -= ActivatePanel;
+    }
+    // Activates the game over panel
+    void ActivatePanel()
+    {
+        respawnPanel.gameObject.SetActive(true);
+        Debug.Log("ULAN");
+    }
+    public void Respawning()
+    {
+        respawnPanelAnimator.SetTrigger("Pressed");
+        OnRespawn?.Invoke();
+    }
+}
